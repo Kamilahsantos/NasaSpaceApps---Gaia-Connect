801849756273ceb111bb0ac102597150adf04f5b
Kamila de fatima santos oliveira <32311268+Kamilahsantos@users.noreply.github.com>
2019-10-20 16:48:25 -0300

    app.py v2

---
+++ backend/spaceapps/app.py
@@ -0,0 +1,98 @@
+from flask import Flask
+from bson.json_util import dumps
+import pymongo 
+from pymongo import MongoClient
+from flask import request
+from bson.json_util import dumps
+import json
+from flask_restful import reqparse, abort, Api, Resource
+import pickle
+import numpy as np
+import dns
+import ast
+import imp
+from model import NLPModel
+
+
+model=open('{caminho do arquivo}/gaia_previsao_chuva.pkl','rb')
+ 
+client = pymongo.MongoClient('import pymongo')
+ 
+helper_module = imp.load_source('*', '{caminho do arquivo}/helper.py')
+model = NLPModel()
+
+client = pymongo.MongoClient("mongodb+srv://spaceapps:<password>@cluster0-jf6fr.mongodb.net/test?retryWrites=true&w=majority") 
+db = client.ContactDB
+collection = db['WeatherEvents']
+
+app = Flask(__name__)
+api = Api(app)
+
+
+#library pkl
+clf_path = 'lib/models/SentimentClassifier.pkl'
+with open(clf_path, 'rb') as f:
+    model.clf = pickle.load(f)
+
+#carregar vetor
+vec_path = 'lib/models/TFIDFVectorizer.pkl'
+with open(vec_path, 'rb') as f:
+    model.vectorizer = pickle.load(f)
+
+parser = reqparse.RequestParser()
+parser.add_argument('query')
+
+
+@app.route("/")
+def home():
+
+        data= json.loads(request.data)
+        Id= data['Id']
+        Temperaturamax= data['temperaturamax'],
+        TemperaturaMin = data['temperaturamin'],
+        Preciptacao = data['preciptacao'],
+        Local = data['local'],
+        Risco= data['risco'],
+        Previsao = data['previsao'],
+        Insolacao = data['insolacao'],
+        Evaporacao_Piche =data ['evaporacao_piche'],
+        Temperaturamedia=  data ['temperaturamedia'],
+        UmidadeRelativa = data ['umidaderelativa'],
+        VelocidadeVento = data ['velocidadeVento'],
+        Timer = data ['timer'],
+        Bairro = data['bairro'],
+
+@app.route("/district", methods = ['GET'])
+def get_district():
+    print ("pagina com todas as cidades")
+
+    try:
+        #lista todos os eventos climáticos que ocorreram nos distrtitos cadastrados
+        return dumps(WeatherEvents)
+        resp.status_code = 200
+
+    except  e:
+        return not_found()
+    if collection.find().count > 0:
+        return dumps(collection.find())
+    else:
+            return jsonify([])
+
+
+
+@app.route("/district/<page_id>", methods = ['GET'])
+def FindById():
+    print ("busca as cidades por id")
+
+    try:
+        district = [district for district in district if district['id'] == district_id]
+        WeatherEvents= db.WeatherEvents.find(Id)
+        return dumps(WeatherEvents)
+
+       
+    except e:
+        return not_found()
+
+    data = json.loads(request.data)
+    WeatherEvents = data['WeatherEvents']  
+
